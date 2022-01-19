954e7ad2b66465d6880326adcbca6a2600dffdab
Tsiegmund <90394665+Tsiegmund@users.noreply.github.com>
2022-01-19 13:32:22 -0500

    created apple.py

---
+++ apple.py
@@ -0,0 +1,6 @@
+import shutil
+
+original = r'C:\Users\Tyler2023411\OneDrive - York County School of Technology\Pictures'
+target = r'C:\Users\Tyler2023411\OneDrive - York County School of Technology\Pictures'
+
+shutil.copyfile(original, target)
